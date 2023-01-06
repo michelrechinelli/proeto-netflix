2f228da105a3f4e13066e40a7f4232444a0f261d
Michel Rechinelli <michel.tupi2@gmail.com>
2023-01-06 17:03:46 -0300

    funcionando o trailer
    criação do trailer

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function toggVideo(){
+function toggleVideo(){
     const trailer = document.querySelector('-trailer');
-    const video = document.querySelector('video');
+    const video = document.querySelector ('video');
     trailer.classList.toggle('active');
 
     video.currentTime = 0;
     video.pause();
-}//problema com o codigo !
+}
