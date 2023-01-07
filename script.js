1b705da49a26dd8cbcc9f8ce22483b8dac78ac8a
Michel Rechinelli <michel.tupi2@gmail.com>
2023-01-06 23:40:24 -0300

    intrudução series
    foi adicionado as landinpages nas treis primeiras series

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleVideo(){
-    const trailer = document.querySelector('-trailer');
-    const video = document.querySelector ('video');
-    trailer.classList.toggle('active');
-
-    video.currentTime = 0;
-    video.pause();
-}
